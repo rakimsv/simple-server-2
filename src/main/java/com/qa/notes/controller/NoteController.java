fb09edd1835ac332495f203bffc7a50c5195d29d
Rakim <rakimsinghverma@gmail.com>
2019-11-20 09:56:06 +0000

    fixed failure

--- src/main/java/com/qa/notes/controller/NoteController.java
+++ src/main/java/com/qa/notes/controller/NoteController.java
@@ -31,7 +31,7 @@ public class NoteController {
 	@Autowired
 	private NoteService service;
 
-	//@RequestMapping(path = "note/", method = {RequestMethod.GET})
+	@RequestMapping(path = "note/", method = {RequestMethod.GET})
 	@ResponseStatus(code = HttpStatus.OK)
 	public List<NoteDto> getNotes(){
 		return new ArrayList<NoteDto>(service.getNotes());
